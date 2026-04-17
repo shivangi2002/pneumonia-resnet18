356f2c1dee26c300aea03f76e442ae489f0ac3c5
shivangi2002 <shivangi.gautam200211@gmail.com>
2026-04-17 23:12:29 +0530

    Implement model setup with ResNet18, freeze pretrained layers, add forward pass and loss computation; verified till it

--- src/train.py
+++ src/train.py
@@ -1,5 +1,8 @@
 from torch.utils.data import DataLoader
 from dataset import XRayDataset
+import torch.nn as nn
 
 dataset = XRayDataset(r"..\train")
 dataloader = DataLoader(dataset, batch_size=32, shuffle=True )
+
+criterion = nn.CrossEntropyLoss()
