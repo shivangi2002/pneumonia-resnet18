5eb0e827356f5104c94f87499b742775fb49a9e0
shivangi2002 <shivangi.gautam200211@gmail.com>
2026-04-23 23:47:54 +0530

    abstracted hyperparmeter to do hyperparmater tuning later

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from src.model import get_model
 from src.train import train_model
 from src.eval import validate_model
 
-def entry_point():
+def run_model(lr, images_per_batch, num_epochs):
     model = get_model()
     
     full_train_dataset = XRayDataset(r"..\data\train")
@@ -22,18 +22,14 @@ def entry_point():
     test_dataset = XRayDataset(r"..\data\test")
     
     
-    
-    
-    
-    
-    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True )
-    val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False )
-    test_loader = DataLoader(test_dataset, batch_size=32, shuffle=True )
+    train_loader = DataLoader(train_dataset, batch_size=images_per_batch, shuffle=True )
+    val_loader = DataLoader(val_dataset, batch_size=images_per_batch, shuffle=False )
+    test_loader = DataLoader(test_dataset, batch_size=images_per_batch, shuffle=True )
 
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     print("Starting training...")
-    train_model(model, train_loader, criterion, optimizer)
+    train_model(model, train_loader, criterion, optimizer, num_epochs)
     
     val_loss, val_accuracy, val_precision, val_recall = validate_model(model, val_loader, criterion)
     print(f"Validation Loss: {val_loss}")
