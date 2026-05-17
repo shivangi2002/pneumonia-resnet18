c46ac48182e2aa78e9e7a3496d6cc2adf1122364
shivangi2002 <shivangi.gautam200211@gmail.com>
2026-05-17 13:34:39 +0530

    refactor fine_tune to fine_tune_layers (0/1/2) for multi-block unfreezing

--- src/model.py
+++ src/model.py
@@ -1,18 +1,21 @@
 import torchvision.models as models
 import torch.nn as nn
 
-def get_model(fine_tune = False):
+def get_model(fine_tune_layers = 0):
     model = models.resnet18(
         weights = models.ResNet18_Weights.DEFAULT,
         )
-    if fine_tune:
-        for name,parm in model.named_parameters():
-            if "layer4" not in name:   
-                parm.requires_grad = False
-        
-    else:
-        for param in model.parameters():
-            param.requires_grad = False
+    
+    for param in model.parameters():
+        param.requires_grad = False
+    
+    if fine_tune_layers >= 1:
+        for param in model.layer4.parameters():
+            param.requires_grad = True
+    
+    if fine_tune_layers >= 2:
+        for param in model.layer3.parameters():
+            param.requires_grad = True
                 
     model.fc = nn.Linear(model.fc.in_features, 2)
     return model
